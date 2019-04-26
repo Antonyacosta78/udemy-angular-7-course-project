e2333d60e89bff9b03387c5cb39c0f6560bf9e64
= <=>
2019-04-26 10:00:31 -0300

    Errorfix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipebook/recipe-list/recipe-list.component';
